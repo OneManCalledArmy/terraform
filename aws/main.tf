db4bcffe445585bb94fbde23e23e8284e2c9c978
OneManCalledArmy <jacaplaca92@gmail.com>
2022-08-08 18:06:37 +0200

    fixed order

--- aws/main.tf
+++ aws/main.tf
@@ -2,12 +2,16 @@ terraform {
   backend "s3" {}
 }
 
-module "vm" {
-    source = "./terraform-modules/vm"
+module "create-sg" {
+    source = "./terraform-modules/create-sg"
 }
 
 module "open-ports" {
     source = "./terraform-modules/open-ports" 
+}
+
+module "vm" {
+    source = "./terraform-modules/vm"
 }
 
 module "run-playbook" {
