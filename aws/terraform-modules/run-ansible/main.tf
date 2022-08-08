ba57c63a0c7b222603d05bca82b8ce2dccae2940
OneManCalledArmy <jacaplaca92@gmail.com>
2022-08-08 21:03:47 +0200

    fixing resource

--- aws/terraform-modules/run-ansible/main.tf
+++ aws/terraform-modules/run-ansible/main.tf
@@ -1,4 +1,4 @@
-resource "run-playbook" "playbook" {
+resource "null_resource" "playbook" {
     provisioner "local-exec" {
         command = "ansible-playbook -i /home/jacek/ansible/inventory/vms.imi /home/jacek/ansible/playbooks/mysql.yaml --private-key ~/.ssh/aws-app.pem -vvv"
     }
