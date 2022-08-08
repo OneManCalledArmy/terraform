16862bbf8fe1190b2bea8eadede9f4eef5951f40
OneManCalledArmy <jacaplaca92@gmail.com>
2022-08-08 19:07:54 +0200

    removing comments

--- aws/terraform-modules/create-sg/main.tf
+++ aws/terraform-modules/create-sg/main.tf
@@ -2,15 +2,7 @@ resource "aws_security_group" "sg" {
   name = "Brand New SG"
   description = "Fresh Security Group"
   vpc_id = "vpc-0d36096a3954e4b62"
-  # for_each = toset([for key in var.ports : tostring(key)])
-
-  # ingress {
-  #   from_port = 0
-  #   to_port = 0
-  #   protocol = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-  
+ 
   egress {
     from_port = 0
     to_port = 0
