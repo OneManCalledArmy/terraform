4afcc6aa39594689a84589620581ab9a324fa7fb
LapekBSF <jacek.motyl@bluesoft.com>
2022-10-16 18:28:31 +0200

    branch/main --> first commit

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,9 +1,14 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # ok
 # clean test
 =======
 # new useless line
 >>>>>>> 8d3d2c3 (change in vars to create conflict)
+=======
+# fkit
+# test main
+>>>>>>> 475e529 (branch/main --> first commit)
 
 variable "region" {
   description = "AWS region"
