864b46716a27783d95a26c25afe8243a7f7137f8
LapekBSF <jacek.motyl@bluesoft.com>
2022-10-16 18:31:29 +0200

    final clean

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-# ok
-# clean test
-=======
-# new useless line
->>>>>>> 8d3d2c3 (change in vars to create conflict)
-=======
-# fkit
-# test main
->>>>>>> 475e529 (branch/main --> first commit)
-
-
 variable "region" {
   description = "AWS region"
   type        = string
